c201ef04c46bf79687dab73b6bea2b9fe11dbfd6
Gareth Rushgrove <gareth@morethanseven.net>
2019-01-01 19:15:14 +0000

    basic tests for CNAB class

---
+++ cnab/test_cnab.py
@@ -0,0 +1,25 @@
+import pytest  # type: ignore
+
+from cnab import CNAB, Bundle
+
+
+@pytest.fixture
+def app():
+    return CNAB("bundle.json")
+
+
+@pytest.mark.parametrize("action", ["install", "upgrade", "uninstall"])
+def test_actions_present(app, action):
+    assert action in app.actions()
+
+
+def test_port_parameter_present(app):
+    assert "port" in app.parameters()
+
+
+def test_app_name(app):
+    assert app.name == "helloworld"
+
+
+def test_app_bundle(app):
+    assert isinstance(app.bundle, Bundle)
